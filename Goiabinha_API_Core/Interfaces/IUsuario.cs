88090d57af7e30601279b124c2acfb2208658829
Rodhenrique <henriquesilvasenai@gmail.com>
2021-02-25 13:54:39 -0300

    GOGO-4: Estrutura básica api

---
+++ Goiabinha_API_Core/Interfaces/IUsuario.cs
@@ -0,0 +1,35 @@
+﻿using Goiabinha_API_Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Goiabinha_API_Core.Interfaces
+{
+    public interface IUsuario
+    {
+        /// <summary>
+        /// Listar todos os usuários
+        /// </summary>
+        List<Usuario> get();
+
+        /// <summary>
+        /// Buscar um usuário
+        /// </summary>
+        Usuario getById(string id);
+
+        /// <summary>
+        /// Cadastrar um usuário
+        /// </summary>
+        void post(Usuario Usuario);
+        /// <summary>
+        /// Atualizar um usuário 
+        /// </summary>
+        void update(string id, Usuario Usuario);
+
+        /// <summary>
+        /// Deletar um usuário
+        /// </summary>
+        void delete(string id);
+    }
+}
---
+++ Goiabinha_API_Core/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Goiabinha_API_Core.Models
+{
+    public class Usuario
+    {
+        public string id { get; set; }
+        public string firstName { get; set; }
+        public string surname { get; set; }
+        public int age { get; set; }
+        public DateTime creationDate { get; set; }
+    }
+}
