6b2c56d61955c8a5fbc6afea5316fed87313ba0c
Rodhenrique <henriquesilvasenai@gmail.com>
2021-03-10 16:39:01 -0300

    GOGO-9: Tela de cadastro finalizada e http post finalizado

---
+++ Henrique H. Silva/Goiabeira_Xamarin_Mobile/Goiabeira_Xamarin_Mobile/Repositories/ServiceRepository.cs
@@ -0,0 +1,26 @@
+﻿using Goiabeira_Xamarin_Mobile.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goiabeira_Xamarin_Mobile.Repositories
+{
+    public class ServiceRepository
+    {
+        public async Task<string> Cadastrar(Usuario NovoUsuario)
+        {
+            using (var client = new HttpClient())
+            {
+                Uri uri = new Uri(string.Format("http://IP_V4:8000/api/Usuario"));
+                var content = new StringContent(JsonConvert.SerializeObject(NovoUsuario), Encoding.UTF8, "application/json");
+                
+                HttpResponseMessage response = await client.PostAsync(uri, content);
+
+                return response.ReasonPhrase;    
+            }
+        }
+    }
+}
