cd530d93870f390069698e71e8332951b0375a0a
Rodhenrique <henriquesilvasenai@gmail.com>
2021-03-10 17:12:39 -0300

    GOGO-9: Adicionando WebApi

---
+++ Henrique H. Silva/Goiabinha_WebApi/Migrations/20210305192320_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Goiabinha_WebApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    id = table.Column<string>(nullable: false),
+                    firstName = table.Column<string>(nullable: true),
+                    surname = table.Column<string>(nullable: true),
+                    age = table.Column<int>(nullable: false),
+                    creationDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
---
+++ Henrique H. Silva/Goiabinha_WebApi/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Goiabinha_WebApi.Context;
+using Goiabinha_WebApi.Interfaces;
+using Goiabinha_WebApi.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+namespace Goiabinha_WebApi
+{
+    public class Startup
+    {    
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+             // Adiciona o MVC ao projeto
+             .AddMvc()
+             .AddJsonOptions(options => {
+                   // Ignora valores nulos ao fazer junções nas consultas
+                   options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                   // Ignora os loopings nas consultas
+                   options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+             })
+
+             // Define a versão do .NET Core
+             .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+            services.AddTransient<IUsuario, UsuarioRepository>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            using (var context = new GoiabinhaContext())
+            {
+                context.Database.Migrate();
+            }
+            
+            app.UseMvc();
+        }
+    }
+}
